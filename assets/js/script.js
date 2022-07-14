3d27496be68cb67009e9fc28f7b2bd181bc7689e
Victoria David <victoria@thedavids.us>
2022-07-14 02:56:32 -0400

    temporary getItem methods until loop is figured out

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,17 @@ $(".saveBtn").on("click", function ()
 //get events from storage to display
 var getDesc = function()
 {
-
+    $("#8am .description").val(localStorage.getItem("8am"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#6pm .description").val(localStorage.getItem("6pm"));
 };
 
 //change textarea colors based on time
