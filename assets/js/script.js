f6c8c58455bb26a5840db7a116a89ff4308e0ac2
Victoria David <victoria@thedavids.us>
2022-07-14 02:00:57 -0400

    localStorage active

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+var hourlyEvents = {};
+
 //date display
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
+
+//activate save button to save to local storage
+$(".saveBtn").on("click", function ()
+{
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+});
+
+//get events from storage to display
+var getEvents = function()
+{
+
+}
+
+//change textarea colors based on time
+var colorTracker = function()
+{
+
+}
