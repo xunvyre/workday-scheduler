75a2fe56b49653e52982ede4567ccc43c06dc092
Victoria David <victoria@thedavids.us>
2022-07-14 02:55:35 -0400

    color tracking added but needs testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,50 @@ $("#currentDay").html(currentDate);
 //activate save button to save to local storage
 $(".saveBtn").on("click", function ()
 {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    var taskDesc = $(this).siblings(".description").val();
+    var descTime = $(this).parent().attr("id");
 
-    localStorage.setItem(time, text);
+    localStorage.setItem(descTime, taskDesc);
 });
 
 //get events from storage to display
-var getEvents = function()
+var getDesc = function()
 {
 
-}
+};
 
 //change textarea colors based on time
 var colorTracker = function()
 {
+    var now = moment().hour();
+    console.log(now);
+    
+    $(".time-block").each(function ()
+    {
+        var descTime = parseInt($(this).attr("id").split("hour"));
 
-}
+        if (descTime < now)
+        {
+            $(".description").removeClass("future");
+            $(".description").removeClass("present");
+            $(".description").addClass("past");
+        }
+        else if (descTime === now)
+        {
+            $(".description").removeClass("past");
+            $(".description").removeClass("future");
+            $(".description").addClass("present");
+        }
+        else
+        {
+            $(".description").removeClass("present");
+            $(".description").removeClass("past");
+            $(".description").addClass("future");
+        }
+
+    });
+};
+
+colorTracker();
+getDesc();
+
